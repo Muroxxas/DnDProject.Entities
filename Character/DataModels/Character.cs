247a486b20036b03ec9217d2773c311356e8a2db
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-03 04:19:32 -0500

    Add project files.

---
+++ Character/DataModels/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Entities.Character.DataModels
+{
+    public class Character
+    {
+        [Key, Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Character_id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid User_id { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        public string CharacterName { get; set; }
+
+        [Required]
+        public Guid Race_id { get; set; }
+
+        [StringLength(45)]
+        public string Alignment { get; set; }
+
+        public int Exp { get; set; }
+
+        public string Background { get; set; }
+
+        public bool Inspiration { get; set; }
+
+        public Character() { }
+    }
+}
