622c1cc6a52245ba5cb6d6a346dd1a6322f123fa
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-03 17:52:28 -0500

    Added Character View Model

--- Character/ViewModels/CharacterVM.cs
+++ Character/ViewModels/CharacterVM.cs
@@ -6,26 +6,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DnDProject.Entities.Character.DataModels
+namespace DnDProject.Entities.Character.ViewModels
 {
-    public class Character
+    public class CharacterVM
     {
         [Key, Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid Character_id { get; set; }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public Guid User_id { get; set; }
-
         [Required]
         [StringLength(45)]
         public string Name { get; set; }
 
-        [Required]
-        public Guid Race_id { get; set; }
-
         [StringLength(45)]
         public string Alignment { get; set; }
+
+        public Guid Race_id { get; set; }
 
         public int Exp { get; set; }
 
@@ -33,6 +29,7 @@ namespace DnDProject.Entities.Character.DataModels
 
         public bool Inspiration { get; set; }
 
-        public Character() { }
+        public CharacterVM() { }
+
     }
 }
