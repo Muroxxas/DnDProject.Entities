0b412532f6246436a8efd263ff71823672e453da
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-04 15:51:06 -0500

    ported stats data model from primary project

---
+++ Character/DataModels/Stats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Entities.Character.DataModels
+{
+    public class Stats
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Character_id { get; set; }
+
+        public int Strength { get; set; }
+
+        public int Dexterity { get; set; }
+
+        public int Constitution { get; set; }
+
+        public int Intelligence { get; set; }
+
+        public int Wisdom { get; set; }
+
+        public int Charisma { get; set; }
+    }
+}
