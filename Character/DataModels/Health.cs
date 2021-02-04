bd306ad9378999aca9992a449d57dbcb745ed538
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-04 14:36:25 -0500

    Ported health data model from the front end project

---
+++ Character/DataModels/Health.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Entities.Character.DataModels
+{
+    public class Health
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Character_id { get; set; }
+
+        [Required]
+        public int MaxHP { get; set; }
+
+        public int CurrentHP { get; set; }
+
+        public int TempHP { get; set; }
+
+        public int DeathSaveSuccesses { get; set; }
+
+        public int DeathSaveFails { get; set; }
+
+        public Health() { }
+    }
+}
