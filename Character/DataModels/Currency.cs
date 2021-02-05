54c3c03109201fb9f41fb4a4134d7493f14470f9
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-05 13:27:25 -0500

    ported currency model from main project

---
+++ Character/DataModels/Currency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Entities.Character.DataModels
+{
+    public class Currency
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Character_id { get; set; }
+
+        public int PlatinumPieces { get; set; }
+
+        public int GoldPieces { get; set; }
+
+        public int ElectrumPieces { get; set; }
+
+        public int SilverPieces { get; set; }
+
+        public int CopperPieces { get; set; }
+    }
+}
