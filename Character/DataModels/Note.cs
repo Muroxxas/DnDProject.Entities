240d7ea9cb45664f0c341de41598074d2c021bf5
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-05 14:52:22 -0500

    ported note model from primary porject

---
+++ Character/DataModels/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Entities.Character.DataModels
+{
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Note_id { get; set; }
+
+        public Guid Character_id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Contents { get; set; }
+
+        public Note() { }
+    }
+}
