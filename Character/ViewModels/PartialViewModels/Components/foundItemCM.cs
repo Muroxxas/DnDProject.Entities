2a62e2dba925db538a0483484f201abae8dd03c1
Jacob Lavarnway <lavarnway932@gmail.com>
2021-03-04 09:48:23 -0500

    a

---
+++ Character/ViewModels/PartialViewModels/Components/foundItemCM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Entities.Character.ViewModels.PartialViewModels.Components
+{
+    public class foundItemCM
+    {
+        public string Name { get; set; }
+
+        public Guid Item_id { get; set; }
+    }
+}
