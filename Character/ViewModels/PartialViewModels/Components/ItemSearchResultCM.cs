092023345fb77478b19be859aa208d1d689a01ae
Jacob Lavarnway <lavarnway932@gmail.com>
2021-03-05 13:46:00 -0500

    Updated the model used by ItemSearchResult

---
+++ Character/ViewModels/PartialViewModels/Components/ItemSearchResultCM.cs
@@ -0,0 +1,20 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Entities.Character.ViewModels.PartialViewModels.Components
+{
+    public class ItemSearchResultCM
+    {
+        public IPagedList<foundItemCM> foundItems { get; set; }
+
+        public string currentFilter { get; set; }
+
+        public string currentGetItemsBy { get; set; }
+
+        public int currentPage { get; set; }
+    }
+}
